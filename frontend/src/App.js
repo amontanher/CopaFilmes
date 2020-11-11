3eb770d06f746e0e00c84b2250bcc0266063fac1
Alessandra <alessandraamontanher@gmail.com>
2020-11-10 22:23:33 -0300

    reading json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import moviesTitles from './data/movies.json';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+    setMovies(moviesTitles);
+  },[])
+
   return (
-    <div className="App">
-      <p>its running...</p>
-    </div>
+    <ul>
+      {movies.map(item => (
+        <li key={item.id}>
+          {item.titulo}
+        </li>
+      ))}
+    </ul>
   );
 }
 
---
+++ frontend/src/services/api.js
@@ -0,0 +1,5 @@
+const moviesURL = "http://copafilmes.azurewebsites.net/api/filmes";
+
+export const getMovies = async () => {
+    return null
+}
