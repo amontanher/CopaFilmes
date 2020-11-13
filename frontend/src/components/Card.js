d17a8b43e323995d69d43f5bb266c8f771cfc6ec
Alessandra <alessandraamontanher@gmail.com>
2020-11-12 21:46:13 -0300

    Implements events (#4)
    * starts handle
    
    * handle checkbox
    
    * implements checkbox
    
    * implements button

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import '../styles/Card.css';
+import React from "react";
+import "../styles/Card.css";
 
-export default function Card({ title, year }) {
-    return (
-        <>
-            <div className="container-card">
-                <div className="container-check">
-                    <p>Check</p>
-                </div>
-                <div className="container-detail">
-                    <h5>{title}</h5>
-                    <h6>{year}</h6>
-                </div>
-            </div>
-        </>
-    );
+export default function Card({
+  item,
+  id,
+  title,
+  year,
+  handleFunction,
+  isChecked,
+  totalSelected,
+  totalMovies
+}) {
+  const disable = totalSelected === totalMovies ? true : false;
+  return (
+    <>
+      <div className="container-card">
+        <div className="container-check">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={(e) => handleFunction(e, item)}
+            value={id}
+            disabled={disable}
+          />
+        </div>
+        <div className="container-detail">
+          <h5>{title}</h5>
+          <h6>{year}</h6>
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,38 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import '../../styles/Home.css';
-import moviesTitles from '../../data/movies.json'
+import React, { useState, useEffect } from "react";
+import "../../styles/Home.css";
+import moviesTitles from "../../data/movies.json";
 
-import Banner from '../../components/Banner';
-import Button from '../../components/Button';
-import Selection from '../../components/Selection';
-import Card from '../../components/Card';
+import Banner from "../../components/Banner";
+import Button from "../../components/Button";
+import Selection from "../../components/Selection";
+import Card from "../../components/Card";
 
 export default function Home() {
-    const [movies, setMovies] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [movies, setMovies] = useState([]);
+  const [totalMovies] = useState(8);
+  const [selectedMovies, setSelectedMovies] = useState([]);
+  const [enableGenerateButton, setEnableGenerateButton] = useState(false);
 
-    useEffect(() => {
-        setMovies(moviesTitles);
-    }, [])
+  const handleGenerateChampionship = () => {
+    console.log(selectedMovies);
+  };
 
-    return (
-        <div className="container">
-            <div>
-                <Banner title="Fase de Seleção"
-                    project="CAMPEONATO DE FILMES"
-                    description="Selecione 8 filmes que você
+  const handleCheckboxChange = (event, item) => {
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      setSelectedMovies([...selectedMovies, item]);
+      setTotal(total => total + 1);
+    } else {
+      const titleIndex = selectedMovies.indexOf(item);
+      selectedMovies.splice(titleIndex, 1);
+
+      setTotal(total => total - 1);
+    }
+
+    enableButton();
+  };
+
+  const enableButton = () => {
+    if (total === 8)
+      setEnableGenerateButton(true);
+    else
+      setEnableGenerateButton(false);
+  }
+
+  useEffect(() => {
+    setMovies(moviesTitles);
+  }, []);
+
+  return (
+    <div className="container">
+
+      <div>
+        <Banner
+          title="Fase de Seleção"
+          project="CAMPEONATO DE FILMES"
+          description="Selecione 8 filmes que você
                 deseja que entrem na competição e depois
                 pressione o botão Gerar Meu Campeonato
-                para prosseguir." />
-            </div>
-            <div className="container-events">
-                <Selection selecteds="0" total="8" />
-                <Button title="Gerar Meu Campeonato" />
-            </div>
-            <div className="container-list">
-                {movies.map(item => (
-                    <Card key={item.id} title={item.titulo} year={item.ano} />
-                ))}
-            </div>
-        </div>
-    );
+                para prosseguir."
+        />
+      </div>
+
+      <div className="container-events">
+        <Selection selecteds={total} total={totalMovies} />
+        {enableGenerateButton && <Button
+          title="Gerar Meu Campeonato"
+          handleFunction={handleGenerateChampionship}
+        />}
+      </div>
+
+      <div className="container-list">
+        {movies.map((item) => (
+          <Card
+            key={item.id}
+            id={item.id}
+            title={item.titulo}
+            year={item.ano}
+            item={item}
+            totalSelected={total}
+            totalMovies={totalMovies}
+            handleFunction={handleCheckboxChange}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
