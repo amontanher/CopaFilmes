5dd99debf4288239c420113453885a4e3dfc0ba7
Alessandra <alessandraamontanher@gmail.com>
2020-11-15 20:12:41 -0300

    assets

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -11,7 +11,7 @@ export default function Card({
   totalSelected,
   totalMovies
 }) {
-  const disable = totalSelected === totalMovies+1 ? true : false;
+  const disable = totalSelected === totalMovies + 1 ? true : false;
   return (
     <>
       <div className="container-card">
