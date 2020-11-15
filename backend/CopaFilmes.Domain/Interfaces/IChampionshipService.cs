c774778e8264707c2be6fc09ed766757bd8df320
Alessandra <alessandraamontanher@gmail.com>
2020-11-15 17:42:33 -0300

    Tests (#9)

--- backend/CopaFilmes.Domain/Interfaces/IChampionshipService.cs
+++ backend/CopaFilmes.Domain/Interfaces/IChampionshipService.cs
@@ -6,8 +6,9 @@ namespace CopaFilmes.Domain.Interfaces
     public interface IChampionshipService
     {
         List<ChampionshipResult> Run(IEnumerable<Movie> movies);
-        List<ChampionshipResult> RunChampionshipPhases(IEnumerable<Movie> movies);
-        List<Movie> StartCurrentChampionshipPhase(List<Movie> movies);
         List<Movie> SetCurrentGame(Movie movieOne, Movie movieTwo);
+        List<Movie> StartCurrentChampionshipPhase(List<Movie> movies);
+        List<ChampionshipResult> RunChampionshipPhases(IEnumerable<Movie> movies);
+        List<ChampionshipResult> StartCurrentChampionshipFinalMatch(List<Movie> semiFinalsRound);
     }
 }
